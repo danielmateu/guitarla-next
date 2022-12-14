07dc60ec10b454104d319b15c9927fc6afea6cff
Daniel Mateu <danielmateu86@gmail.com>
2022-12-14 14:39:31 +0100

    Rutas y Link

---
+++ pages/nosotros.js
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+
+const Nosotros = () => {
+    return (
+        <>
+            <h1>Nosotros</h1>
+
+            <Link href="/">Vuelve a inicio</Link>
+        </>
+    )
+}
+
+export default Nosotros
