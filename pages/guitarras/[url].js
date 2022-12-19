63a2c93a8296b233303201e511cdd9444e6bcfbf
Daniel Mateu <danielmateu86@gmail.com>
2022-12-19 12:25:33 +0100

    Creando el objeto del elemento agregado

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useState } from "react";
 
 import { Layout } from "../../components/Layout";
 import styles from "../../styles/guitarras.module.css";
@@ -6,7 +7,31 @@ import styles from "../../styles/guitarras.module.css";
 
 const Producto = ({ guitarra }) => {
 
+    const [cantidad, setCantidad] = useState(0);
     const { nombre, descripcion, imagen, precio } = guitarra[0].attributes
+
+    // console.log(guitarra[0])
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(cantidad < 1 ){
+            alert('Cantidad no válida');
+            return;
+        }
+
+        //Contstruir objeto con la guitarra seleccionada y almacenar en localstorage
+        const guitarraSeleccionada = {
+            id: guitarra[0].id,
+            imagen: imagen.data.attributes.url,
+            nombre,
+            precio,
+            cantidad
+        }
+
+        // console.log(guitarraSeleccionada)
+
+        //Pasando la info al context
+    }
 
     return (
         <Layout
@@ -18,7 +43,29 @@ const Producto = ({ guitarra }) => {
                     <h3>{nombre}</h3>
                     <p className={styles.descripcion}>{descripcion}</p>
                     <p className={styles.precio}>${precio}</p>
-                    {/* <button>Añadir</button> */}
+
+                    <form
+                        action=""
+                        className={styles.formulario}
+                        onSubmit={handleSubmit}
+                        >
+                        
+                        <label htmlFor="cantidad">Cantidad: </label>
+
+                        <select 
+                            onChange={e => setCantidad(+e.target.value)}
+                            name="cantidad" id="cantidad">
+                            <option value="0">--Seleccione--</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+
+                        <input type="submit" value='agregar al carrito'/>
+                    </form>
+
                 </div>
             </div>
         </Layout>
